a190097843b6bd31c05964226701c7feb8b25cb4
artisanweb4-blip <artisanweb4-blip@github.com>
2026-09-16 15:26:03 +0000

    nettoyage: supprime dossier dupliqué

--- projet_tailleur/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Client, Mensuration, Tissu, Commande, Depense
-
-admin.site.register(Client)
-admin.site.register(Mensuration)
-admin.site.register(Tissu)
-admin.site.register(Commande)
-admin.site.register(Depense)
